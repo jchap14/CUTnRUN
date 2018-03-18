5d83f1e2cff67e1bf6079c0e7465e539d2a39611
James <jchap14@gmail.com>
2018-03-17 23:04:37 -0700

    update

--- PeakCalling/run_BAM2BDG_indiv.sh
+++ PeakCalling/run_BAM2BDG_indiv.sh
@@ -1,55 +1,35 @@
 #!/bin/bash
-# This script is how to submit the "BAM2BDG_indiv.py" script
-
-##### submit for all sorted yeast bams in CWD (do in screen + QLOGIN for now))
-## submit 
-# cp /srv/gsfs0/projects/snyder/chappell/scripts/CUTnRUN/BAM2BDG_indiv.py .
-# screen
-# qlogin -l h_vmem=10G -l h_rt=24:00:00
-# bash run_BAM2BDG.sh
-
-## this doesn't work yet, for now use above to run on all BAMs at once
-# for x in `/bin/ls *.nmSort.bam` ; do bash run_BAM2BDG.sh $x; done
-
-## add modules
-source activate CUT_n_RUN
-module add ucsc_tools/2.7.2
-
-## run script
-python BAM2BDG.py
-
-## deactivate conda environment
-source deactivate
-
-######## 
-
-#!/bin/bash
 # This is the submission script for "BAM2BDG_indiv.py"
 
-# submit for a specific bedgraph
+# submit for a specific bamfile
 # bash run_BAM2BDG_indiv.sh $SAMPLE.nmSort.bam
 
 ##### submit for all sorted bedgraphs in CWD
 # for x in `/bin/ls *.nmSort.bam` ; do bash run_BAM2BDG_indiv.sh $x; done
+
+##### add modules
+source activate CUT_n_RUN
+module add ucsc_tools/2.7.2
+
 
 ##### specify variables to pass to BAM2BDG_indiv.py
 BAMFILE=$1
 NAME=`basename $BAMFILE .nmSort.bam`
 SPIKEFILE=`echo $NAME.yst.bam`
 MERGE_CLOSE_PEAKS="True"
-ends=False
-lengths_analysis=True
-lengths_image=True
-size_select_1=True
-size_select_2=True
-bedgraph=True
-chrom_sizes="hg19"
-big_wig=True
-size_min_1=20
-size_max_1=120
-size_min_2=150
-size_max_2=710
-multiplying_factor=10000
+ENDS=False
+LENGTHS_ANALYSIS=True
+LENGTHS_IMAGE=True
+SIZE_SELECT_1=True
+SIZE_SELECT_2=True
+BEDGRAPH=True
+CHROM_SIZES="hg19"
+BIG_WIG=True
+SIZE_MIN_1=20
+SIZE_MAX_1=120
+SIZE_MIN_2=150
+SIZE_MAX_2=710
+MULTIPLYING_FACTOR=10000
 
 ## create tempscript
 cat > $NAME.tempscript.sh << EOF
@@ -69,16 +49,24 @@ source activate CUT_n_RUN
 module add ucsc_tools/2.7.2
 
 ## run script
-python /srv/gsfs0/projects/snyder/chappell/scripts/CUTnRUN/BAM2BDG_indiv.py \
-                    --bedgraph $BEDGRAPH \
-                    --threshold $THRESHOLD \
-                    --min_length $MIN_LENGTH \
-                    --inter_peak_distance $INTER_PEAK_DISTANCE \
-                    --merge_close_peaks $MERGE_CLOSE_PEAKS \
-                    --max_length $MAX_LENGTH \
-                    --generate_ID $GENERATE_ID
-
-python /srv/gsfs0/projects/snyder/chappell/scripts/CUTnRUN/PeakCalling/BAM2BDG.py \
+python /srv/gsfs0/projects/snyder/chappell/scripts/CUTnRUN/PeakCalling/BAM2BDG_indiv.py \
+--bamfile $BAMFILE \
+--name $NAME \
+--spikefile $SPIKEFILE \
+--merge_close_peaks $MERGE_CLOSE_PEAKS \
+--ends $ENDS \
+--lengths_analysis $LENGTHS_ANALYSIS \
+--lengths_image $LENGTHS_IMAGE \
+--size_select_1 $SIZE_SELECT_1 \
+--size_select_2 $SIZE_SELECT_2 \
+--bedgraph $BEDGRAPH \
+--chrom_sizes $CHROM_SIZES \
+--big_wig $BIG_WIG \
+--size_min_1 $SIZE_MIN_1 \
+--size_max_1 $SIZE_MAX_1 \
+--size_min_2 $SIZE_MIN_2 \
+--size_max_2 $SIZE_MAX_2 \
+--multiplying_factor $MULTIPLYING_FACTOR
 
 ## deactivate conda environment
 source deactivate
@@ -87,5 +75,5 @@ EOF
 ## qsub then remove the tempscript
 qsub $NAME.tempscript.sh
 sleep 1
-rm $NAME.tempscript.sh
+# rm $NAME.tempscript.sh
 
