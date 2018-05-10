eef4c6c6c2013c06bd3a10b2e934e0f8fb119395
James <jchap14@gmail.com>
2018-05-09 18:05:51 -0700

    -peak_caller macs2

--- Kundaje_ChIPseq_CnR.sh
+++ Kundaje_ChIPseq_CnR.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ##### Submission script for BDS controlled ChIPseq pipeline
-## for x in `/bin/ls *.trim.R1.fq.gz` ; do bash Kundaje_ChIPseq.sh $x; done
+## for x in `/bin/ls *.trim.R1.fq.gz` ; do bash Kundaje_ChIPseq_CnR.sh $x; done
 
 ##### INPUTs required
 ## type of factor for ChIP (-type in python script)
-# CHIPTYPE="histone"
-CHIPTYPE="TF"
+CHIPTYPE="histone"
+# CHIPTYPE="TF"
 
 ##### input files to pass to chipseq.py (FQs should be trimmed)
 ## calc replicate 1 FQs (format == $NAME.repB)
@@ -23,12 +23,14 @@ cat > $NAME.tempscript.sh << EOF
 
 ##### run script
 ## -type can be histone or TF
+## forcing macs2 for peak calling
+## maybe xcor can be turned off, but then need to set -extsize_macs2 [EXTSIZE]
 
 python /srv/gsfs0/projects/snyder/chappell/TF_chipseq_pipeline/chipseq.py \
 -type $CHIPTYPE --screen $NAME -system slurm -q_for_slurm_account -q mpsnyder \
--pe -species hg19 -nth 12 \
+-pe -species hg19 -nth 12 -peak_caller macs2 \
 -fastq1_1 $FQ_R1 -fastq1_2 $FQ_R2 \
--out_dir $NAME -mem_dedup 50G -mem_bwa 50G -mem_spp 20G
+-out_dir $NAME -mem_dedup 80G -mem_bwa 50G -mem_spp 20G -mem_macs2 25G
 
 ## deactivate conda environment
 EOF
